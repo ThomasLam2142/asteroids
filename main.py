d7d9b08b99d53308f812c70fedbe3535c35ebda5
thomaslam2142 <thomaslam2142@gmail.com>
2025-02-05 21:48:09 -0500

    added player.py and player instance in main

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    #Start pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    Clock = pygame.time.Clock()
+    dt = 0
 
+    new_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)
 
+    #Main Loop
     while True:
         print("Game loop start")
+
+        new_player.draw(screen)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+
+        Clock.tick(60)
+        dt = (Clock.tick() // 1000)
+        print("Game loop End")
 
 if __name__ == "__main__":
     main()
