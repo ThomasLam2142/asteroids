eb4485af41d791ae417b37fc58502778ac8cd5d9
thomaslam2142 <thomaslam2142@gmail.com>
2025-02-05 21:22:40 -0500

    added main game loop

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+    while True:
+        print("Game loop start")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
