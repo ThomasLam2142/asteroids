4d1148453405f4fdf057a4f5acba0292afab1fcc
thomaslam2142 <thomaslam2142@gmail.com>
2025-02-06 23:06:19 -0500

    Finished L6

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
-
-    #Start pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    Clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    new_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)
-
-    #Main Loop
     while True:
-        print("Game loop start")
-
-        new_player.draw(screen)
-
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        Clock.tick(60)
-        dt = (Clock.tick() // 1000)
-        print("Game loop End")
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
